ee1f6a308061bfd16b04bcb932ab1b5a1def815d
impana29 <impanasomashekar29@gmail.com>
2019-10-13 15:26:10 +0530

    uploading session folder

---
+++ session/app.js
@@ -0,0 +1,45 @@
+//console.log("Iam new user");
+//console.error("occured error");
+//console.warn("its a warning");
+//alert("Iam alert");
+
+//console.log(name+" is my topic");
+
+/*if(true){
+    var name="javascript";
+ console.log(name+ " inside if");
+}
+console.log(name+ " outside if");
+
+let letvar = 'iam outside';
+if(true)
+{
+    let letvar ='iam inside';
+    console.log(letvar)
+}
+console.log(letvar);
+function sjcit() {
+    alert('iam function');
+}
+sjcit();
+
+var name='vtu'
+console.log(name);
+function sjcit(){
+    let name='sjcit'
+    console.log(name+" inside sjcit")
+    }
+sjcit();
+function vtu(){
+    console.log(name+ " inside vtu")
+    console.log(window)
+}
+vtu();*/
+function myFunction(elem){
+    console.log(elem)
+    elem.style.color="red"
+}
+function aFunction(elem){
+    console.log(elem)
+    elem.style.console="sjcit"
+}
