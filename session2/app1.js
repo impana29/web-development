6231c28e344bab20ab9fbad2a704115b27d2f606
impana29 <impanasomashekar29@gmail.com>
2019-10-26 14:06:22 +0530

    adding files

---
+++ session2/app1.js
@@ -0,0 +1,9 @@
+function myFunct() {
+    alert();
+}
+function myFunct1() {
+//debugger
+console.log(event.target.value);
+document.getElementById("changed").innerHTML=event.target.value;
+//console.log(event.clientX+ ':'+event.clientY);
+}
